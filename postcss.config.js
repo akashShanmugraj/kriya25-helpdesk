15c21f9a26de9f42b1d7e5ac92c3b03c8ac97514
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2025-01-26 15:47:17 +0530

    revert to `postcss7-compat` with updated version (`2.1.4`)

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // filepath: /Users/akashshanmugaraj/Documents/Personal Projects/kriya/kriya25-helpdesk/postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss8-compat': {},
+    '@tailwindcss/postcss7-compat': {},
     autoprefixer: {},
   },
 };
