1cad8cad57c8922014478fabdad9f47069588dab
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2025-01-26 14:56:19 +0530

    update Dockerfile and package.json for port change and tailwind compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// filepath: /Users/akashshanmugaraj/Documents/Personal Projects/kriya/kriya25-helpdesk/postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss7-compat': {},
+    '@tailwindcss/postcss8-compat': {},
     autoprefixer: {},
   },
 };
