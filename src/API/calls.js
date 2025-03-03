fef0fe40a671c7774030e096f8126f1edebed468
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2025-03-03 09:29:12 +0530

    update API base URL to point to the new backend

--- src/API/calls.js
+++ src/API/calls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://kriyadb.psgtech.ac.in/api";
+export const BASE_URL = "https://kriyabackend.psgtech.ac.in/api";
 
 export const AUTH_URL = `${BASE_URL}/convenor-auth`;
 export const REGISTER_URL = `${BASE_URL}/register`;
